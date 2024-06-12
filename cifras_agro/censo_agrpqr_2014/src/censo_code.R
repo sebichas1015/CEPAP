6d03cbe8efc76c81b3a0b247888507c8365fdc71
sebichas1015 <sebastian.phyp@gmail.com>
2024-06-12 12:00:27 -0500

    se elimina archivo

--- cifras_agro/censo_agrpqr_2014/src/censo_code.R
+++
@@ -1,8 +0,0 @@
-# Authors: Sebas and Nicholas
-# Year: 2024
-
-pacman::p_load(readr, dplyr, tidyr)
-
-input_censo <- "CEPAP/Total_nacional(csv)/S01_15(Unidad_productora).csv"
-
-prueba <- read_csv(input_censo)
