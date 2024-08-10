27fc5c57df692fe6037eedcc3e7320c5376d1edd
sebichas1015 <sebastian.phyp@gmail.com>
2024-08-10 00:49:28 -0500

    se completa el script

--- cifras_agro/igac_gini/process/src/import_clean.jl
+++ cifras_agro/igac_gini/process/src/import_clean.jl
@@ -5,55 +5,56 @@
 # =========================================
 # CEPAP/team_data/cifras_agro/igac_gini/input/GINI_MUNICIPAL_PERCENTILES.csv
 
-using Pkg, DataFrames, Tables, CSV, StatsBase, Dates, Arrow, Random, StringEncodings, Transliterate
+using Pkg, DataFrames, CSV, Random, Dates, StringEncodings, Transliterate, Arrow
 
 Random.seed!(19481210)
 
 println("load functions--", now())
-function replace_commas(x)
-    return [ismissing(item) ? missing : replace(item, "," => ".") for item in x]
-end
-
-function coerce_deci(x)
-    return [ismissing(item) ? missing : parse(Float64, item) for item in x]
-end
+replace_commas(x) = [ismissing(item) ? missing : replace(item, "," => ".") for item in x]
+coerce_deci(x) = [ismissing(item) ? missing : parse(Float64, item) for item in x]
 
 println("load data--", now())
 input_1 = "C:/Users/sebas/OneDrive/Documents/CEPAP/team_data/cifras_agro/igac_gini/process/input/GINI_MUNICIPAL_PERCENTILES.csv"
-
 gini_igac = DataFrame(CSV.File(open(input_1, enc"ISO-8859-1")))
 
-println("clean names--", now())
-names_gini = names(gini_igac)
+println("clean column names--", now())
+rename!(gini_igac, Symbol.(replace.(lowercase.(names(gini_igac)), r"\s+|\.+|__+" => "_")))
+rename!(gini_igac, replace.(names(gini_igac), "__" => "_"))
 
-names_clean = [
-    lowercase(replace(replace(replace(item, " " => "_"), "." => "_"), "__" => "_")) for item in names_gini
-    ]
-
-rename!(gini_igac, names_clean)
-
-println("clean values--", now())
+println("count missing values--", now())
 n_missng_gini = count(ismissing, gini_igac.gini)
-
 n_missng_area_has = count(ismissing, gini_igac.area_ha)
 
+println("clean values--", now())
 string_cols = names(gini_igac, String)
-
 transform!(gini_igac, string_cols .=> ByRow(transliterate) .=> string_cols)
-
 cols_numrc = ["area_ha", "gini"]
-
-transform!(gini_igac, cols_numrc .=> replace_commas .=> cols_numrc)
-
-transform!(gini_igac, cols_numrc .=> coerce_deci .=> cols_numrc)
-
-@assert count(ismissing, gini_igac.gini) == n_missng_gini
-
-@assert count(ismissing, gini_igac.area_ha) == n_missng_area_has
+transform!(gini_igac, cols_numrc .=> (col -> coerce_deci(replace_commas(col))) .=> cols_numrc)
+@assert count(ismissing, gini_igac.gini) == n_missng_gini && count(ismissing, gini_igac.area_ha) == n_missng_area_has
 
 println("filter--", now())
+gini_igac_perc = gini_igac[.!ismissing.(gini_igac.percentiles), :]
+@assert all(combine(groupby(gini_igac_perc, :cod_muni), :cod_muni => length => :n_recs).n_recs .== 300)
+gini_igac = gini_igac[ismissing.(gini_igac.percentiles), :]
+@assert all(combine(groupby(gini_igac, :cod_muni), :cod_muni => length => :n_recs).n_recs .== 3)
 
+println("select cols--", now())
+n_cols_gini_igac_perc = ncol(gini_igac_perc)
+n_col_igac = ncol(gini_igac)
+vars_selct = setdiff(names(gini_igac), ["no_propietarios", "no_predios", "area_ha",
+"area_m2", "disparidad_superior"])
+select!(gini_igac_perc, vars_selct)
+@assert ncol(gini_igac_perc) == n_cols_gini_igac_perc - 5
+vars_selct = setdiff(names(gini_igac), ["percentiles"])
+select!(gini_igac, vars_selct)
+@assert ncol(gini_igac) == n_col_igac - 1
 
+println("export--", now())
+output_1 = "C:/Users/sebas/OneDrive/Documents/CEPAP/team_data/cifras_agro/igac_gini/process/output/gini_igac.parquet"
+output_2 = "C:/Users/sebas/OneDrive/Documents/CEPAP/team_data/cifras_agro/igac_gini/process/output/gini_igac_perc.parquet"
+Arrow.write(output_1, gini_igac)
+Arrow.write(output_2, gini_igac_perc)
+gini_igac = nothing
+gini_igac_perc = nothing
 
-
-
+println("done import_clean.jl--", now())
