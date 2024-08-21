e4f364073438352d5e8f7f210d4df5d9be176ad6
sebichas1015 <sebastian.phyp@gmail.com>
2024-08-21 00:35:44 -0500

    se adicionan calculos con la proporcion de coca sobre el area del municipio

--- requerimiento/gini_coca/src/code_gini_coca.R
+++ requerimiento/gini_coca/src/code_gini_coca.R
@@ -6,7 +6,7 @@
 # CO-JXXXXXXXXXX
 
 pacman::p_load(argparse, here, dplyr, logger, arrow, assertr, openxlsx, janitor,
-               tidyr, stringr)
+               tidyr, stringr, sf)
 
 setwd("/Users/sebas/OneDrive/Documents/CEPAP/team_data/")
 
@@ -16,7 +16,7 @@ parser$add_argument("--coca_2022",
 parser$add_argument("--gini_muni",
                     default = "cifras_agro/igac_gini/process/output/gini_igac.parquet")
 parser$add_argument("--munis_area",
-                    default = "munis_col_2021/process/output/shapefile_munis.arrow")
+                    default = "munis_col_2021/process/input/SHP_MGN2021_COLOMBIA/MGN_2021_COLOMBIA/ADMINISTRATIVO/MGN_MPIO_POLITICO.shp")
 args <- parser$parse_args()
 
 log_info("load functions")
@@ -33,38 +33,58 @@ coerce_number <- function(x) {
 }
 
 log_info("load data")
+table_shape_munis <- read_sf(args$munis_area) %>% 
+  clean_names() %>% 
+  assert(is_uniq, mpio_cdpmp) %>% 
+  select(mpio_cdpmp, mpio_narea)
+
 coca_2022 <- read_parquet(args$coca_2022)
 
 gini_muni <- read_parquet(args$gini_muni,
-                          col_select = all_of(c("cod_muni", "gini", "area_de_estudio")))
+                          col_select =
+                            all_of(c("cod_muni", "gini", "area_de_estudio"))) %>% 
+  assert(not_na, cod_muni)
 
 log_info("filter")
 gini_muni <- gini_muni %>% 
   filter(area_de_estudio == "PREDIOS RURALES PRIVADOS") %>% 
+  filter(cod_muni > 5000) %>% 
   assert(is_uniq, cod_muni)
 
 log_info("standardize values")
+table_shape_munis <- table_shape_munis %>% 
+  mutate(mpio_cdpmp = coerce_number(mpio_cdpmp))
+
+gini_muni <- gini_muni %>% 
+  verify(all(cod_muni %in% table_shape_munis$mpio_cdpmp))
+
 coca_2022 <- coca_2022 %>% 
-  mutate(codmpio = coerce_number(codmpio))
+  mutate(codmpio = coerce_number(codmpio)) %>% 
+  verify(all(codmpio %in% table_shape_munis$mpio_cdpmp))
 
 log_info("join and clean values")
 n_gini_muni <- nrow(gini_muni)
 
 gini_muni <- gini_muni %>% 
   left_join(coca_2022, by = c("cod_muni" = "codmpio")) %>% 
+  left_join(table_shape_munis, by = c("cod_muni" = "mpio_cdpmp")) %>% 
   verify(nrow(.) == n_gini_muni) %>% 
-  mutate(anio_2022 = case_when(is.na(anio_2022) ~ 0, TRUE ~ anio_2022))
+  mutate(anio_2022 = case_when(is.na(anio_2022) ~ 0, TRUE ~ anio_2022)) %>% 
+  mutate(prop_coca = anio_2022/(mpio_narea*100)) %>% 
+  mutate(prop_coca = round(prop_coca, 5))
 
 prueba <- gini_muni %>% 
   filter(anio_2022 > 0) %>% 
   filter(gini > 0)
 
-lm_1 <- lm(gini_muni$gini ~gini_muni$anio_2022)
+corr_1 <- cor(gini_muni$gini, gini_muni$anio_2022)
 
-lm_2 <- lm(prueba$gini ~prueba$anio_2022)
+corr_2 <- cor(prueba$gini, prueba$anio_2022)
 
+na_preuba <- gini_muni %>% 
+  filter(is.na(prop_coca))
 
+corr_3 <- cor(gini_muni$gini, gini_muni$prop_coca)
 
-
-
+corr_4 <- cor(prueba$gini, prueba$prop_coca)
 
