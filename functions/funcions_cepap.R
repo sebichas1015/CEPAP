3da464e60e6797d8c5536eabba02b173acab7ba7
sebichas1015 <sebastian.phyp@gmail.com>
2024-08-24 12:06:31 -0500

    se carga carpeta para funciones2

---
+++ functions/funcions_cepap.R
@@ -0,0 +1,14 @@
+#
+# Authors:     SCB
+# Maintainers: SCB
+# Copyright: 2024, JEP, GPL or newer
+# =========================================
+# CO-JXXXXXXXXXX
+
+clean_names_munis <- function(shp) {
+  
+  shp <- shp %>% 
+    clean_names() %>% 
+}
+
+
