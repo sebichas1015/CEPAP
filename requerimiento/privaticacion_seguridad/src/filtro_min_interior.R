52188bc651745d0817df4018c5d3f38f5e89dd61
sebichas1015 <sebastian.phyp@gmail.com>
2025-07-23 22:05:05 -0500

    add new scripts

--- requerimiento/privaticacion_seguridad/src/filtro_min_interior.R
+++ requerimiento/privaticacion_seguridad/src/filtro_min_interior.R
@@ -1,19 +1,42 @@
 
-pacman::p_load(openxlsx, dplyr, writexl, stringr, janitor, stringi)
+#
+# Authors:     SCB
+# Maintainers: SCB
+# Copyright: 2025, CEPAP
+# =========================================
+# requerimientos/
 
-path <- "/Users/sebas/OneDrive/Documents/curso_r_cs/input_data/archivo_historico/Catálogo Min Interior 1948-2003.xlsx"
+pacman::p_load(readxl, dplyr, writexl, stringr, janitor, stringi, logger, here,
+                argparse, writexl, purrr, digest)
 
-df_mn <- read.xlsx(path) %>% 
+log_info("define arguments")
+parser <- ArgumentParser()
+parser$add_argument("--input_minintr",
+                    default = "/mnt/c/Users/sebas/OneDrive/Documentos/CEPAP/data_original/archivo/min_interior/Catálogo Min Interior 1948-2003.xlsx")
+parser$add_argument("--output_filt",
+                    default = "/mnt/c/Users/sebas/OneDrive/Documentos/CEPAP/requerimiento/privaticacion_seguridad/output/consulta_min_interior.xlsx")
+args <- parser$parse_args()
+
+
+log_info("read data")
+df_mn <- read_excel(args$input_minintr,
+                   col_types = "text") %>% 
   clean_names()
 
 df_mn_fil <- df_mn %>% 
   mutate(asunto_o_tema = str_to_lower(asunto_o_tema)) %>% 
   mutate_if(is.character, ~stri_trans_general(., "latin-ascii"))
   
-filtros <- "seguridad privada|convivir|vigilancia|privatizacion|coopetativa de vigilancia|connivencia"
+filtros <- "seguridad privada|convivir|vigilancia|autodefensa|paramilita"
 
-no_filtros <- "electora|fondo de vigilancia bogota"
+no_filtros <- "electora|fondo de vigilancia bogota|fondo vigilancia bogota"
 
 df_filt <- df_mn_fil %>% 
   filter(str_detect(asunto_o_tema, filtros)) %>%
   filter(!(str_detect(asunto_o_tema, no_filtros)))
+
+log_info("export")
+write_xlsx(df_filt, args$output_filt)
+
+log_info("done filtro_min_interior.R")
+#done
--- requerimiento/privaticacion_seguridad/src/filtro_presidencia.R
+++ requerimiento/privaticacion_seguridad/src/filtro_presidencia.R
@@ -1,19 +1,31 @@
 
-pacman::p_load(openxlsx, dplyr, writexl, stringr, janitor, stringi)
+pacman::p_load(readxl, dplyr, writexl, stringr, janitor, stringi, logger, here,
+                argparse, writexl, purrr, digest)
 
-path <- "/Users/sebas/OneDrive/Documents/curso_r_cs/input_data/archivo_historico/Inventario-Documental-Archivo-Central-DAPRE.xlsx"
+log_info("define arguments")
+parser <- ArgumentParser()
+parser$add_argument("--input_presdnc",
+                    default = "/mnt/c/Users/sebas/OneDrive/Documentos/CEPAP/data_original/archivo/presidencia/Inventario-Documental-Archivo-Central-DAPRE.xlsx")
+parser$add_argument("--output_filt",
+                    default = "/mnt/c/Users/sebas/OneDrive/Documentos/CEPAP/requerimiento/privaticacion_seguridad/output/consulta_presidencia.xlsx")
+args <- parser$parse_args()
 
-df_mn <- read.xlsx(path, startRow = 3) %>% 
+df_mn <- read_excel(args$input_presdnc,
+                   col_types = "text",
+                   skip = 2) %>% 
   clean_names()
 
 df_mn_fil <- df_mn %>% 
   mutate(nombre_expediente = str_to_lower(nombre_expediente)) %>% 
   mutate_if(is.character, ~stri_trans_general(., "latin-ascii"))
 
-filtros <- "seguridad privada|convivir|vigilancia|privatizacion|coopetativa de vigilancia|connivencia"
+filtros <- "seguridad privada|convivir|vigilancia|autodefensa|paramilita"
 
-no_filtros <- "electora|fondo de vigilancia bogota"
+no_filtros <- "electora|fondo de vigilancia bogota|fondo vigilancia bogota"
 
 df_filt <- df_mn_fil %>% 
   filter(str_detect(nombre_expediente, filtros)) %>%
   filter(!(str_detect(nombre_expediente, no_filtros)))
+
+
+
